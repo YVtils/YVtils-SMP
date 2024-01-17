4ec22466ddf3bf588db8f8bf751a0b6a378403ed
WolfiiYV <minecraftwandler@gmail.com>
2024-01-17 12:55:19 +0100

    mojangAPI

--- src/main/kotlin/yv/tils/smp/utils/MojangAPI.kt
+++ src/main/kotlin/yv/tils/smp/utils/MojangAPI.kt
@@ -1,4 +1,17 @@
 package yv.tils.smp.utils
 
 class MojangAPI {
+    fun name2uuid(playerName: String): UUID {
+        try {
+            val url = "https://api.mojang.com/users/profiles/minecraft/${playerName}"
+            val connection = new URL(url).openConnection()
+            connection.setRequestMethod("GET")
+
+            val responseCode = connection.getResponseCode()
+        }
+        catch(Exception e) {
+            
+        }
+        return null
+    }
 }
