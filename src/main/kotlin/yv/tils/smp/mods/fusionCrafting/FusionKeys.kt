7259cdd2239f96e0676268c7931b9b7a81c42247
WolfiiYV <minecraftwandler@gmail.com>
2024-07-06 22:12:18 +0200

    Add new namespaced key for fusion filtering

--- src/main/kotlin/yv/tils/smp/mods/fusionCrafting/FusionKeys.kt
+++ src/main/kotlin/yv/tils/smp/mods/fusionCrafting/FusionKeys.kt
@@ -6,6 +6,7 @@ import yv.tils.smp.YVtils
 enum class FusionKeys(val key: NamespacedKey) {
     FUSION_GUI(NamespacedKey(YVtils.instance, "fusion_gui")),
     FUSION_ITEMNAME(NamespacedKey(YVtils.instance, "fusion_itemname")),
+    FUSION_CURRENT_FILTER(NamespacedKey(YVtils.instance, "fusion_current_filter")),
     FUSION_INVISIBLE(NamespacedKey(YVtils.instance, "fusion_invisible")),
     FUSION_PLAYER_HEAD(NamespacedKey(YVtils.instance, "fusion_player_head"))
 }
