4757077f669dbd550a3f0c3578338b9c6a13d443
WolfiiYV <minecraftwandler@gmail.com>
2024-07-06 22:18:11 +0200

    Add click listeners for new inventorys

--- src/main/kotlin/yv/tils/smp/mods/fusionCrafting/GUIListener.kt
+++ src/main/kotlin/yv/tils/smp/mods/fusionCrafting/GUIListener.kt
@@ -9,6 +9,7 @@ import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 import yv.tils.smp.mods.fusionCrafting.enchantments.PlayerHeadLoad
+import yv.tils.smp.mods.fusionCrafting.manager.FusionManagerGUI
 import yv.tils.smp.utils.color.ColorUtils
 import yv.tils.smp.utils.logger.Debugger
 
@@ -18,81 +19,243 @@ class GUIListener {
         val inv = e.inventory
 
         if (player.openInventory.title() == ColorUtils().convert("<gold>Fusion Crafting") && e.inventory.location == null) {
-            e.isCancelled = true
+            clickOverview(e, player)
+        } else if (player.openInventory.title() == ColorUtils().convert("<red>Fusion Management") && e.inventory.location == null) {
+            clickManagement(e, player)
+        } else if (ColorUtils().convert(player.openInventory.title()).startsWith("<gold>Fusion Crafting - ") && e.inventory.location == null) {
+            clickCrafting(e, player, inv)
+        } else if (player.openInventory.title() == ColorUtils().convert("<gold>Fusion Manager") && e.inventory.location == null) {
+            clickManager(e, player)
+        }
+    }
 
-            val slot = e.slot
+    private fun clickOverview(e: InventoryClickEvent, player: HumanEntity) {
+        e.isCancelled = true
 
-            when (slot) {
-                11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 38, 39, 40, 41, 42 -> {
-                    if (e.currentItem == null) return
-                    if (e.currentItem!!.type == Material.LIGHT_GRAY_STAINED_GLASS_PANE) return
+        val slot = e.slot
 
-                    val questName = FusionLoader.component2name[e.currentItem!!.displayName()]!!
-                    val questMap = FusionLoader().loadFusion(questName)
+        when (slot) {
+            11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 38, 39, 40, 41, 42 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.LIGHT_GRAY_STAINED_GLASS_PANE) return
 
-                    Debugger().log(
-                        "Opened fusion",
-                        "Name: $questName | Map: $questMap",
-                        "yv.tils.smp.mods.fusionCrafting.GUIListener"
-                    )
+                val fusionName = FusionLoader.component2name[e.currentItem!!.displayName()]!!
+                val fusionMap = FusionLoader().loadFusion(fusionName)
 
-                    FusionCraftingGUI().fusionGUI(player, questMap)
-                }
+                Debugger().log(
+                    "Opened fusion",
+                    "Name: $fusionName | Map: $fusionMap",
+                    "yv.tils.smp.mods.fusionCrafting.GUIListener"
+                )
 
-                45 -> {
-                    if (e.currentItem == null) return
-                    if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
-                    println("This would go to the last page")
-                }
-
-                53 -> {
-                    if (e.currentItem == null) return
-                    if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
-                    println("This would go to the next page")
-                }
+                FusionCraftingGUI().fusionGUI(player, fusionMap)
             }
-        } else if (ColorUtils().convert(player.openInventory.title())
-                .startsWith("<gold>Fusion Crafting - ") && e.inventory.location == null
-        ) {
-            e.isCancelled = true
 
-            val slot = e.slot
+            45 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
+                FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting", getCurrentPage(e.inventory) - 1)
+            }
 
-            val acceptSlots = listOf(47, 48, 49, 50, 51, 52)
-            val backSlot = 45
+            53 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
+                FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting", getCurrentPage(e.inventory) + 1)
+            }
 
-            when (slot) {
-                in acceptSlots -> {
-                    FusionCheck().buildItemList(player = player as Player, inv = inv)
+            49 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
 
-                    if (!FusionCheck.canAccept) return
+                var tags = FusionLoader.tagMap.keys.toMutableList()
+                tags = tags.sorted().toMutableList()
 
-                    for (item in FusionCheck.fusionItems) {
-                        player.inventory.removeItem(item)
+                val currentTag = e.currentItem!!.itemMeta.persistentDataContainer.get(FusionKeys.FUSION_CURRENT_FILTER.key, PersistentDataType.STRING)
+
+                val clickType = e.click
+                val index = tags.indexOf(currentTag)
+
+                if (clickType.isLeftClick) {
+                    if (index == tags.size - 1) {
+                        FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting", 1)
+                    } else {
+                        FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting", 1, tags[index + 1])
                     }
-
-                    var i = 0
-                    while (i < 6) {
-                        when (i) {
-                            0 -> addToInventory(player, checkOutput(15, inv))
-                            1 -> addToInventory(player, checkOutput(16, inv))
-                            2 -> addToInventory(player, checkOutput(24, inv))
-                            3 -> addToInventory(player, checkOutput(25, inv))
-                            4 -> addToInventory(player, checkOutput(33, inv))
-                            5 -> addToInventory(player, checkOutput(34, inv))
-                            else -> break
-                        }
-                        i++
+                } else if (clickType.isRightClick) {
+                    if (index == 0) {
+                        FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting", 1)
+                    } else {
+                        FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting", 1, tags[index - 1])
                     }
-
-                    FusionCheck().buildItemList(player = player, inv = inv)
-                }
-
-                backSlot -> {
-                    FusionCraftingGUI().generateGUI(player)
                 }
             }
         }
+    }
+
+    private fun clickManagement(e: InventoryClickEvent, player: HumanEntity) {
+        e.isCancelled = true
+
+        val slot = e.slot
+
+        when (slot) {
+            11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 38, 39, 40, 41, 42, 8 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.LIGHT_GRAY_STAINED_GLASS_PANE) return
+
+                val fusionName: String = if (slot != 8) {
+                    FusionLoader.component2name[e.currentItem!!.displayName()]!!
+                } else {
+                    "null"
+                }
+
+                Debugger().log(
+                    "Opened fusion for management",
+                    "Name: $fusionName | Player: ${player.name}",
+                    "yv.tils.smp.mods.fusionCrafting.GUIListener"
+                )
+
+                FusionManagerGUI().openInventory(player as Player, fusionName)
+            }
+
+            45 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
+                FusionOverview().openOverview(player as Player, "<red>Fusion Management", getCurrentPage(e.inventory) - 1)
+            }
+
+            53 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
+                FusionOverview().openOverview(player as Player, "<red>Fusion Management", getCurrentPage(e.inventory) + 1)
+            }
+
+            49 -> {
+                if (e.currentItem == null) return
+                if (e.currentItem!!.type == Material.GRAY_STAINED_GLASS_PANE) return
+
+                var tags = FusionLoader.tagMap.keys.toMutableList()
+                tags = tags.sorted().toMutableList()
+
+                val currentTag = e.currentItem!!.itemMeta.persistentDataContainer.get(FusionKeys.FUSION_CURRENT_FILTER.key, PersistentDataType.STRING)
+
+                val clickType = e.click
+                val index = tags.indexOf(currentTag)
+
+                if (clickType.isLeftClick) {
+                    if (index == tags.size - 1) {
+                        FusionOverview().openOverview(player as Player, "<red>Fusion Management", 1)
+                    } else {
+                        FusionOverview().openOverview(player as Player, "<red>Fusion Management", 1, tags[index + 1])
+                    }
+                } else if (clickType.isRightClick) {
+                    if (index == 0) {
+                        FusionOverview().openOverview(player as Player, "<red>Fusion Management", 1)
+                    } else {
+                        FusionOverview().openOverview(player as Player, "<red>Fusion Management", 1, tags[index - 1])
+                    }
+                }
+            }
+        }
+    }
+
+    private fun clickCrafting(e: InventoryClickEvent, player: HumanEntity, inv: Inventory) {
+        e.isCancelled = true
+
+        val slot = e.slot
+
+        val acceptSlots = listOf(47, 48, 49, 50, 51, 52)
+        val backSlot = 45
+
+        when (slot) {
+            in acceptSlots -> {
+                FusionCheck().buildItemList(player = player as Player, inv = inv)
+
+                if (!FusionCheck.canAccept) return
+
+                for (item in FusionCheck.fusionItems) {
+                    player.inventory.removeItem(item)
+                }
+
+                var i = 0
+                while (i < 6) {
+                    when (i) {
+                        0 -> addToInventory(player, checkOutput(15, inv))
+                        1 -> addToInventory(player, checkOutput(16, inv))
+                        2 -> addToInventory(player, checkOutput(24, inv))
+                        3 -> addToInventory(player, checkOutput(25, inv))
+                        4 -> addToInventory(player, checkOutput(33, inv))
+                        5 -> addToInventory(player, checkOutput(34, inv))
+                        else -> break
+                    }
+                    i++
+                }
+
+                FusionCheck().buildItemList(player = player, inv = inv)
+            }
+
+            backSlot -> {
+                FusionOverview().openOverview(player as Player, "<gold>Fusion Crafting")
+            }
+        }
+    }
+
+    private fun clickManager(e: InventoryClickEvent, player: HumanEntity) {
+        e.isCancelled = true
+
+        val slot = e.slot
+
+        val toggleSlot = 0
+        val displayItemSlot = 11
+        val nameSlot = 12
+        val descriptionSlot = 13
+        val tagsSlot = 14
+        val fusionInvSlot = 15
+        val backSlot = 18
+        val deleteSlot = 26
+
+        when (slot) {
+            toggleSlot -> {
+                println("This would toggle the fusion")
+            }
+
+            displayItemSlot -> {
+                println("This would open a GUI with the display item")
+            }
+
+            nameSlot -> {
+                println("This would open a anvil GUI to change the name")
+            }
+
+            descriptionSlot -> {
+                println("This would close the GUI temporarily and prompt the player toi write the description in chat")
+            }
+
+            tagsSlot -> {
+                println("This would open a GUI to change the tags")
+                /*
+                Each tag own Slot or Anvil GUI and Tags need to be seperated with space, comma or semicolon
+                 */
+            }
+
+            fusionInvSlot -> {
+                println("This would open the fusion inventory")
+            }
+
+            deleteSlot -> {
+                println("This would delete the fusion")
+            }
+
+            backSlot -> {
+                FusionOverview().openOverview(player as Player, "<red>Fusion Management")
+            }
+        }
+    }
+
+    private fun getCurrentPage(inv: Inventory): Int {
+        val item = inv.getItem(4) ?: return 1
+        val meta = item.itemMeta ?: return 1
+
+        return meta.displayName()?.let { ColorUtils().convert(it) }?.split(" ")?.get(1)?.toInt() ?: 1
     }
 
     private fun addToInventory(player: HumanEntity, item: ItemStack) {
