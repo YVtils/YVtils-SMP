03868d9c26d90238dc82227a55de21ce2564fd04
WolfiiYV <minecraftwandler@gmail.com>
2024-07-30 18:45:20 +0200

    - Add function to delete fusions - Finish function to save Fusion

--- src/main/kotlin/yv/tils/smp/mods/fusionCrafting/manager/FusionManagerGUI.kt
+++ src/main/kotlin/yv/tils/smp/mods/fusionCrafting/manager/FusionManagerGUI.kt
@@ -87,7 +87,7 @@ class FusionManagerGUI {
         return Fusion(state, thumbnail, name, description, tags, fusionInv, fileName)
     }
 
-    // TODO: Saves in file, but does not work in lifetime without restart
+    // TODO: When the fusion is new and no input/output is set a error will be thrown when trying to edit again
     fun setData(fusion: Fusion) {
         val file = File(YVtils.instance.dataFolder.path, "fusions/${fusion.fileName}.yml")
         val ymlFile: YamlConfiguration = YamlConfiguration.loadConfiguration(file)
@@ -104,40 +104,24 @@ class FusionManagerGUI {
             val key = fusionItem.key
             val value = fusionItem.value
 
-            // TODO: Implement logic to set data
-            /*
-            [23:15:36 INFO]: Input: [{item=ITEM_FRAME}, {amount=1}, {data=}]
-[23:15:36 INFO]: Input: [{item=GLOW_ITEM_FRAME}, {amount=1}, {data=}]
-[23:15:36 INFO]: Input: [{item=Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=White_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Orange_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Magenta_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Light_Blue_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Yellow_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Lime_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Pink_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Gray_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Light_Gray_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Cyan_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Purple_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Blue_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Brown_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Green_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Red_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Input: [{item=Black_Stained_Glass_Pane}, {amount=4}, {data=}]
-[23:15:36 INFO]: Output: [{item=ITEM_FRAME}, {amount=4}, {name=<gold>Invisible Item Frame}, {lore=<white>Place this Item Frame and it will be invisible! <newline><red>Empty Item Frames will be destroyed and dropped after one minute!}, {data=Invisible}]
-
-             */
+            // TODO: Try again when saving the fusion recipe if it really works
             if (key.startsWith("input.")) {
-                println("Input: $value")
-//                ymlFile.set("input", value) // TODO: Should work like this but not sure
+                ymlFile.set(key, value)
             } else if (key.startsWith("output.")) {
-                println("Output: $value")
-//                ymlFile.set("output", value) // TODO: Should work like this but not sure
+                ymlFile.set(key, value)
             }
         }
 
         ymlFile.save(file)
+
+        FusionLoader().loadFusionThumbnail()
+    }
+
+    fun deleteFusion(player: Player) {
+        val fusion = playerManager[player.uniqueId] ?: return
+
+        val file = File(YVtils.instance.dataFolder.path, "fusions/${fusion.fileName}.yml")
+        file.delete()
 
         FusionLoader().loadFusionThumbnail()
     }
