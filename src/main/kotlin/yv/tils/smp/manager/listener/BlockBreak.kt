3ea4b2572d9251f6efc7fffd74beb59d8964fa39
WolfiiYV <minecraftwandler@gmail.com>
2024-08-17 15:08:33 +0200

    Test: Add function to check if the blockBreakEvent was cancelled. (In respect to world protection plugins)

--- src/main/kotlin/yv/tils/smp/manager/listener/BlockBreak.kt
+++ src/main/kotlin/yv/tils/smp/manager/listener/BlockBreak.kt
@@ -1,13 +1,14 @@
 package yv.tils.smp.manager.listener
 
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import yv.tils.smp.mods.multiMine.MultiMineHandler
 
 class BlockBreak : Listener {
-    @EventHandler
-    fun onBlockBreak(e: BlockBreakEvent) {
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    fun onBlockBreakHigh(e: BlockBreakEvent) {
         MultiMineHandler().trigger(e)
     }
 }
