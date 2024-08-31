ea0ed97d0f0c1be6492a1dac45b969909bc4e22b
WolfiiYV <minecraftwandler@gmail.com>
2024-08-31 22:56:50 +0200

    Add function to get Name by FusionKeys

--- src/main/kotlin/yv/tils/smp/mods/fusionCrafting/enchantments/DataTags.kt
+++ src/main/kotlin/yv/tils/smp/mods/fusionCrafting/enchantments/DataTags.kt
@@ -5,4 +5,10 @@ import yv.tils.smp.mods.fusionCrafting.FusionKeys
 enum class DataTags(val key: FusionKeys, val desc: String) {
     INVISIBLE(FusionKeys.FUSION_INVISIBLE, ""),
     PLAYER_HEAD(FusionKeys.FUSION_PLAYER_HEAD, ""),
+
+    ;companion object {
+        fun getNameByKey(key: FusionKeys): String? {
+            return entries.find { it.key == key }?.name
+        }
+    }
 }
