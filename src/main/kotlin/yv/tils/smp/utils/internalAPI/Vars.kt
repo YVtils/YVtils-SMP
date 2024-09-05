1a1adecfbb1b3d73c8c90972653c53bd504b5110
WolfiiYV <minecraftwandler@gmail.com>
2024-09-05 18:39:53 +0200

    Remove whitespace before prefix

--- src/main/kotlin/yv/tils/smp/utils/internalAPI/Vars.kt
+++ src/main/kotlin/yv/tils/smp/utils/internalAPI/Vars.kt
@@ -3,6 +3,6 @@ package yv.tils.smp.utils.internalAPI
 import yv.tils.smp.utils.configs.global.Config
 
 class Vars {
-    var prefix = " <dark_gray>[<blue>YVtils-SMP<dark_gray>]<white>"
+    var prefix = "<dark_gray>[<blue>YVtils-SMP<dark_gray>]<white>"
     var prefixCustom = " ${Config.config["prefix"]}"
 }
