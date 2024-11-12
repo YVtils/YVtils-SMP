646cb17b6990d843300301489ca7d4a5cc121186
WolfiiYV <minecraftwandler@gmail.com>
2024-11-12 15:26:30 +0100

    Add (Stripped) Mangrove Log

--- src/main/kotlin/yv/tils/smp/utils/configs/multiMine/config_yml_multiMine.kt
+++ src/main/kotlin/yv/tils/smp/utils/configs/multiMine/config_yml_multiMine.kt
@@ -30,6 +30,7 @@ class config_yml_multiMine {
             Material.ACACIA_LOG.name,
             Material.DARK_OAK_LOG.name,
             Material.CHERRY_LOG.name,
+            Material.MANGROVE_LOG.name,
             Material.CRIMSON_STEM.name,
             Material.WARPED_STEM.name,
             Material.STRIPPED_OAK_LOG.name,
@@ -39,6 +40,7 @@ class config_yml_multiMine {
             Material.STRIPPED_ACACIA_LOG.name,
             Material.STRIPPED_DARK_OAK_LOG.name,
             Material.STRIPPED_CHERRY_LOG.name,
+            Material.STRIPPED_MANGROVE_LOG.name,
             Material.STRIPPED_CRIMSON_STEM.name,
             Material.STRIPPED_WARPED_STEM.name,
 
