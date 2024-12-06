1d848a9032d484a020c5240ad6ae05964bb81f7d
WolfiiYV <minecraftwandler@gmail.com>
2024-12-06 16:01:01 +0100

    Remove already done todo

--- src/main/kotlin/yv/tils/smp/mods/multiMine/MultiMineHandler.kt
+++ src/main/kotlin/yv/tils/smp/mods/multiMine/MultiMineHandler.kt
@@ -19,7 +19,6 @@ import java.util.UUID
 
 // TODO: Add fast Leave decay
 // TODO: Try making the break process better for the performance
-// TODO: Add command to toggle multiMine and save in save.yml
 
 class MultiMineHandler {
     companion object {
